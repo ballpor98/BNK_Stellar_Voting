aba9c6be4b3056d33e177dce6d6d6af9e8de77d3
Chawin <chawin.k@ku.th>
2018-11-22 19:29:28 +0700

    Updates

---
+++ precache-manifest.f721228ffda430beb47ab0aa68e35362.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/bnk48/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/bnk48/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/bnk48/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/bnk48/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "55351891711120dd12b7",
+    "url": "/bnk48/static/js/runtime~main.55351891.js"
+  },
+  {
+    "revision": "9e114bc0e6350098278a",
+    "url": "/bnk48/static/js/main.9e114bc0.chunk.js"
+  },
+  {
+    "revision": "eb2dde7b0783e83afa32",
+    "url": "/bnk48/static/js/1.eb2dde7b.chunk.js"
+  },
+  {
+    "revision": "9e114bc0e6350098278a",
+    "url": "/bnk48/static/css/main.b70e4a2b.chunk.css"
+  },
+  {
+    "revision": "eb2dde7b0783e83afa32",
+    "url": "/bnk48/static/css/1.6899bf20.chunk.css"
+  },
+  {
+    "revision": "74fc84af6c4f21a5d8f30b7993b0457e",
+    "url": "/bnk48/index.html"
+  }
+];
